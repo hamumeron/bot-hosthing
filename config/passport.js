64f5d624036cfe95dca6d3120e2c581613baa581
hamumeron <nekosuke.1012@outlook.jp>
2025-06-21 17:27:02 +0900

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,23 +1,18 @@
 const passport = require("passport");
 const DiscordStrategy = require("passport-discord").Strategy;
-const db = require("../services/db"); // データベース操作
+const db = require("../services/db"); // あなたのDBモジュールに応じて
 
 passport.use(new DiscordStrategy({
-  clientID: process.env.DISCORD_ID,
-  clientSecret: process.env.DISCORD_SECRET,
+  clientID: process.env.DISCORD_CLIENT_ID,
+  clientSecret: process.env.DISCORD_CLIENT_SECRET,
   callbackURL: process.env.CALLBACK_URL,
   scope: ['identify']
 }, async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await db.findOrCreateUser(profile); // DiscordのIDでユーザー作成
-    return done(null, user);
-  } catch (err) {
-    return done(err, null);
-  }
+  const user = await db.findOrCreateUser(profile);
+  return done(null, user);
 }));
 
 passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser(async (id, done) => {
-  const user = await db.getUserById(id);
-  done(null, user);
+passport.deserializeUser((id, done) => {
+  db.getUserById(id).then(user => done(null, user));
 });
