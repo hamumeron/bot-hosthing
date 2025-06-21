50db2d2ffceb2f7eea9ea226ea8fb254b0f12771
hamumeron <nekosuke.1012@outlook.jp>
2025-06-21 17:26:36 +0900

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,17 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 
-// Discordにリダイレクト
 router.get("/discord", passport.authenticate("discord"));
 
-// Discordからのコールバック
-router.get("/discord/callback",
-  passport.authenticate("discord", { failureRedirect: "/" }),
-  (req, res) => {
-    res.redirect("/dashboard.html");
-  }
-);
+router.get("/discord/callback", passport.authenticate("discord", {
+  failureRedirect: "/"
+}), (req, res) => {
+  res.redirect("/dashboard.html");
+});
 
-// ログアウト
 router.get("/logout", (req, res) => {
   req.logout(() => res.redirect("/"));
 });
 
 module.exports = router;
+
